fef2d6e44581f8af5b9d95d68ba42775550213ef
misterk7_- <merezkhaan@gmail.com>
2025-05-05 14:35:04 +0100

    Add background

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
-class player{
-  constructor(){
-    this.x = 100;
-    this.y = 100;
-    this.size = 10;
+class Player {
+  constructor(x=0, y=0, size=0) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
   }
-
-
 }
 
+class initWorld {
+  constructor(bkgrnd, x, y, size) {
+    this.bgImage = bkgrnd;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+
+  draw() {
+    image(this.bgImage, this.x, this.y, this.size, this.size);
+  }
+}
+
+let bgImage=p5.Image;
+
+function preload() {
+  bgImage = loadImage('/images/background/Biggergrass_bg.png');
+}
 
 function setup() {
   createCanvas(672, 672);
@@ -15,5 +31,6 @@ function setup() {
 
 function draw() {
   background(220);
-  // hello
+  initialWorld = new initWorld(bgImage, 0,0,672);
+  initialWorld.draw(bgImage, 0, 0);
 }
