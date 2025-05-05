93cc87b6d8c8f1bbbecb7116bb470afc06aa5405
misterk7_- <merezkhaan@gmail.com>
2025-05-05 15:11:16 +0100

    Refactor Player class to extend Entity and improve structure

--- sketch.js
+++ sketch.js
@@ -1,10 +1,62 @@
-class Player {
-  constructor(x=0, y=0, size=0) {
+class Entity {
+  constructor(x,y,width,height,sprite) {
     this.x = x;
     this.y = y;
-    this.size = size;
+    this.width = width;
+    this.height = height;
+    this.sprite = sprite;
+  }
+
+  draw() {
+    image(this.bgImage, this.x, this.y, this.size, this.size);
   }
 }
+
+class Player extends Entity {
+  constructor() {
+    this.x = x;
+    this.y = y;
+    this.size = 36;
+  }
+
+  display(){
+    image(this.sprite, this.xPos, this.yPos, this.tileSize, this.tileSize);
+  }
+
+    // check if a player is pressing one of the WSAD keys
+  // set two variables to either 1 or -1 depending on which direction of the key held down.
+  setDirection(dir){
+    let up = 87;
+    let down = 83;
+    let left = 65;
+    let right = 68;
+
+    if (!this.isMoving){
+      this.isMoving = true;
+
+      if (keyIsDown(up)){
+        this.dirY = -1;
+        this.dirX = 0;
+      }
+      if (keyIsDown(down)){
+        this.dirY = 1;
+        this.dirX = 0;
+      }
+      if (keyIsDown(left)){
+        this.dirX = -1;
+        this.dirY = 0;
+      }
+      if (keyIsDown(right)){
+        this.dirX = 1;
+        this.dirY = 0;
+      }
+      
+      this.checkTargetTile();
+    }
+  }
+}
+
+// TODO: Add a sprite sheet to animate the player or other entities
 
 class initWorld {
   constructor(bkgrnd, x, y, size) {
