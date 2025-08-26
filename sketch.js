8d973011ba88ae76989e36bd2a31fd7772fb556c
misterk7_- <merezkhaan@gmail.com>
2025-08-26 17:40:01 +0100

    Basic functionality setup and tweaks

--- sketch.js
+++ sketch.js
@@ -1,39 +1,38 @@
 class Entity {
-  constructor(x,y,width,height,sprite) {
+  constructor(sprite, x, y, size) {
+    this.sprite = sprite;
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
-    this.sprite = sprite;
+    this.size = size;
   }
 
+  update(){}
+  
   draw() {
-    image(this.bgImage, this.x, this.y, this.size, this.size);
+    image(this.sprite, this.x, this.y, this.size, this.size);
   }
 }
 
 class Player extends Entity {
-  constructor() {
-    this.x = x;
-    this.y = y;
-    this.size = 36;
+  constructor(sprite, x, y, size) {
+    super(sprite, x, y, size);
+    this.dirX = 0;
+    this.dirY = 0;
+    this.isMoving = false;
   }
 
-  display(){
-    image(this.sprite, this.xPos, this.yPos, this.tileSize, this.tileSize);
-  }
-
-    // check if a player is pressing one of the WSAD keys
+  // check if a player is pressing one of the WSAD keys
   // set two variables to either 1 or -1 depending on which direction of the key held down.
-  setDirection(dir){
+  update(){
     let up = 87;
     let down = 83;
     let left = 65;
     let right = 68;
 
+    this.isMoving = false
     if (!this.isMoving){
       this.isMoving = true;
-
+      
       if (keyIsDown(up)){
         this.dirY = -1;
         this.dirX = 0;
@@ -50,8 +49,11 @@ class Player extends Entity {
         this.dirX = 1;
         this.dirY = 0;
       }
-      
-      this.checkTargetTile();
+    }
+    // move the player using defined directions
+    if (this.dirX !== 0 || this.dirY !== 0) {
+      this.x += this.dirX * 3; // speed
+      this.y += this.dirY * 3;
     }
   }
 }
@@ -71,19 +73,24 @@ class initWorld {
   }
 }
 
-let bgImage=p5.Image;
+let bgImage = p5.Image;
 
 function preload() {
   bgImage = loadImage('/images/background/Biggergrass_bg.png');
+  plyrImage = loadImage('/images/enemies/enemy3_frame1.png')
 }
 
 function setup() {
   createCanvas(672, 672);
-  // hey
+  initialWorld = new initWorld(bgImage, 0, 0, 672);
+  plyr = new Player(plyrImage, width/2, height/2, 100);
+  print(width/2);
 }
+
 
 function draw() {
   background(220);
-  initialWorld = new initWorld(bgImage, 0,0,672);
-  initialWorld.draw(bgImage, 0, 0);
+  initialWorld.draw();
+  plyr.update();
+  plyr.draw();
 }
